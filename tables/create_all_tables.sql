0ad3d16bd59bd7f4907800313b59c48cef43f306
Max Dragaev <dragaev@MacBook-Air-Max.local>
2021-03-02 23:37:48 +0700

    add new scripts and folders

---
+++ tables/create_all_tables.sql
@@ -0,0 +1,80 @@
+CREATE TABLE orders
+(id SERIAL PRIMARY KEY,
+ shop_id integer,
+ employee_id integer,
+ total_amount money,
+ date timestamp,
+ FOREIGN KEY (shop_id) REFERENCES shops (id) on delete cascade,
+ FOREIGN KEY (employee_id) REFERENCES employees (id) on delete cascade
+);
+CREATE TABLE orders_items
+(id SERIAL PRIMARY KEY,
+ order_id integer,
+ product_id integer,
+ quantity integer,
+ quantity_type_id integer,
+ amount money,
+ discount integer,
+ total_sum money,
+ FOREIGN KEY (order_id) REFERENCES orders (id) on delete cascade,
+ FOREIGN KEY (product_id) REFERENCES products (id) on delete cascade,
+ FOREIGN KEY (quantity_type_id) REFERENCES quantity_types (id) on delete cascade
+);
+CREATE TABLE products
+(id SERIAL PRIMARY KEY,
+ group_id integer,
+ price money,
+ quantity_type_id integer,
+ name varchar(30),
+ FOREIGN KEY (group_id) REFERENCES products_groups (id) on delete cascade,
+FOREIGN KEY (quantity_type_id) REFERENCES quantity_types (id) on delete cascade
+);
+CREATE TABLE products_groups
+(id SERIAL PRIMARY KEY,
+ group_name varchar(20),
+ parent_group_id integer
+);
+CREATE TABLE quantity_types
+(id SERIAL PRIMARY KEY,
+ type varchar(20)
+);
+CREATE TABLE discounts
+(id SERIAL PRIMARY KEY,
+ type_id integer,
+ product_id integer,
+ group_id integer,
+ from timestamp,
+ to timestamp,
+ percent integer,
+ fixed_sum money,
+ FOREIGN KEY (product_id) REFERENCES products (id) on delete cascade,
+FOREIGN KEY (type_id) REFERENCES discount_types (id) on delete cascade
+);
+CREATE TABLE discount_types
+(id SERIAL PRIMARY KEY,
+ type varchar(20)
+);
+CREATE TABLE shops
+(id SERIAL PRIMARY KEY,
+ name varchar(30),
+ address varchar(30)
+);
+CREATE TABLE employees
+(id SERIAL PRIMARY KEY,
+ f varchar(15),
+ i varchar(15),
+ o varchar(15),
+ position varchar(20),
+ manager_id integer,
+ shop_id integer,
+ FOREIGN KEY (shop_id) REFERENCES shops (id) on delete cascade
+);
+
+CREATE TABLE shops_managers
+(id SERIAL PRIMARY KEY,
+ shop_id integer,
+ manager_id integer,
+ FOREIGN KEY (manager_id) REFERENCES employees (id) on delete cascade,
+ FOREIGN KEY (shop_id) REFERENCES shops (id) on delete cascade
+);
+
