85970cf84e037667b52b94c793e35820d40cc87e
Max Dragaev <dragaev@MacBook-Air-Max.local>
2021-03-05 19:32:39 +0700

    add stored procedures

--- procedures/get_products_from_products.sql
+++ procedures/get_products_from_products.sql
@@ -1,1 +1,11 @@
-select * from
+create procedure get_products_from_products()
+    language plpgsql
+as
+$$
+begin
+    select * from products;
+end;
+$$;
+
+alter procedure get_products_from_products() owner to postgres;
+
